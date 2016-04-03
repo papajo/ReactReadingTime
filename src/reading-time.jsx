e9d6436d30d655872d1512fa9fa5db28d6d93218
papajo <joshipv2@gmail.com>
2016-04-02 21:04:38 -0400

    refactoring

--- src/reading-time.jsx
+++ src/reading-time.jsx
@@ -36,11 +36,14 @@ export default class ReadingTime extends React.Component {
     }
 
     render() {
+      const { props } = this;
+      const { readTime } = this.state;
+      const minutes = readTime === 1? 'minute': 'minutes';
       return (
-        <div>
+        <div {...props}>
           <p>
             Estimeated Read Time: <br /><br />
-            <span>{this.state.readTime}</span>
+            <span>{readTime} {minutes}</span>
           </p>
         </div>
       );
